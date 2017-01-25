87133eeb5722ce1df2f1120a9e5c72b1c2f988c3
Taka Isaksen <taka@Takas-MacBook-Air.local>
2017-01-24 21:51:13 -0500

    added first row to csv output

--- resident/filings.py
+++ resident/filings.py
@@ -15,42 +15,42 @@ filings_head = []
 count = 0
 
 for information in root.findall('./*'):
-    print(information.tag)
+    xmlns = '{http://www.sec.gov/edgar/document/thirteenf/informationtable}'
     info = []
     shrsOrPrnAmt = []
     voting_authority = []
 
     '''setting up the head'''
-    # if count == 0:
-    #     name_of_issuer = information.find('nameOfIssuer').tag
-    #     filings_head.append(name_of_issuer)
-    #     title_of_class = information.find('titleOfClass').tag
-    #     filings_head.append(title_of_class)
-    #     cusip = information.find('cusip').tag
-    #     filings_head.append(cusip)
-    #     value = information.find('value').tag
-    #     filings_head.append(value)
-    #     shrs_or_prn_amt = information.find('shrsOrPrnAmt').tag
-    #     filings_head.append(shrs_or_prn_amt)
-    #     investment_discretion = information.find('investmentDiscretion').tag
-    #     filings_head.append(investment_discretion)
-    #     voting_authority = information.find('votingAuthority').tag
-    #     filings_head.append(voting_authority)
-    #     csvwriter.writerow(filings_head)
-    #     count = count + 1
+    if count == 0:
+        name_of_issuer = information.find(xmlns+'nameOfIssuer').tag
+        filings_head.append(name_of_issuer)
+        title_of_class = information.find(xmlns+'titleOfClass').tag
+        filings_head.append(title_of_class)
+        cusip = information.find(xmlns+'cusip').tag
+        filings_head.append(cusip)
+        value = information.find(xmlns+'value').tag
+        filings_head.append(value)
+        shrs_or_prn_amt = information.find(xmlns+'shrsOrPrnAmt').tag
+        filings_head.append(shrs_or_prn_amt)
+        investment_discretion = information.find(xmlns+'investmentDiscretion').tag
+        filings_head.append(investment_discretion)
+        voting_authority = information.find(xmlns+'votingAuthority').tag
+        filings_head.append(voting_authority)
+        csvwriter.writerow(filings_head)
+        count = count + 1
 
     '''parse through xml for data values'''
-    # name_of_issuer = information.find('nameOfIssuer').text
+    # name_of_issuer = information.find(xmlns+'nameOfIssuer').text
     # info.append(name_of_issuer)
-    # title_of_class = information.find('titleOfClass').text
+    # title_of_class = information.find(xmlns+'titleOfClass').text
     # info.append(title_of_class)
-    # cusip = information.find('cusip').text
+    # cusip = information.find(xmlns+'cusip').text
     # info.append(cusip)
-    # value = information.find('value').text
+    # value = information.find(xmlns+'value').text
     # info.append(value)
-    # investment_discretion = information.find('investmentDiscretion').text
+    # investment_discretion = information.find(xmlns+'investmentDiscretion').text
     # info.append(investment_discretion)
-    # voting_authority = information.find('votingAuthority').text
+    # voting_authority = information.find(xmlns+'votingAuthority').text
     # info.append(voting_authority)
     # csvwriter.writerow(filings_head)
     # count = count + 1
