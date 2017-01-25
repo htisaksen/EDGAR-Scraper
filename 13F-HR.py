a9162580e798b40bf9fe5b9776942033c2469ceb
Hyosun Takaaki Isaksen <ttkay123@gmail.com>
2017-01-25 11:38:11 -0500

    updated 13F

--- 13F-HR.py
+++ 13F-HR.py
@@ -35,31 +35,3 @@ if __name__=="__main__":
     organization = Scraper('0001166559')
     print(organization.getXml(sample_route))
     print(organization.parse_deliminate())
-
-# r= requests.get('http://www.investopedia.com/categories/bonds.asp')
-#
-# soup = BeautifulSoup(r.text, 'html.parser')
-#
-# item_title = [a['href'] for a in soup.find_all('a',attrs={'data-cat': 'content_list'})]
-# print(item_title)
-# print(type(item_title))
-# frontdump=[]
-# backdump=[]
-# dictionary = {}
-# for i in item_title:
-#     time.sleep(.5)
-#     r1=[]
-#     r1 = requests.get('http://www.investopedia.com'+i)
-#     soup=BeautifulSoup(r1.text, 'html.parser')
-#     front=soup.find('h1').text
-#     lasertarget=soup.find('div',attrs={'class': 'content-box content-box-term'}).find('p').text
-#     back=re.match(r'[^\n]+',lasertarget).group()
-#     frontdump.append(front)
-#     backdump.append(back)
-#
-#
-# dictionary = dict(zip(frontdump, backdump))
-#
-#
-# with open('fixedincome.pkl', 'wb') as f:
-#     pickle.dump(dictionary, f)
